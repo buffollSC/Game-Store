f445c62a2c3348f9ba4547a76a147a895836d4ab
Roman <buffollSC@gmail.com>
2022-02-20 17:25:06 +0300

    set and get database

--- src/components/game/game-price/game-price.jsx
+++ src/components/game/game-price/game-price.jsx
@@ -2,20 +2,47 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteItemFromCart, setItemInCart } from '../../../redux/cart/reducer';
 import { BtnBuy } from '../../buttons/btn-buy';
+import { getDatabase, ref, set, child, get, push  } from "firebase/database";
 import './game-price.css';
 
 export const GamePrice = ({ game }) => {
     const dispatch = useDispatch();
     const items = useSelector(state => state.cart.itensInCart);
+    const { email, id } = useSelector((state) => state.user);
     const isItemInCart = items.some(item => item.id === game.id)
+
+    const dataBase = getDatabase();
+    const cartListRef = ref(dataBase, "user: " + id);
+    const newCartRef = push(cartListRef);
+   
   
     const handlerClick = (event) => {
         event.stopPropagation();
+        event.preventDefault();
         if(isItemInCart) {
             dispatch(deleteItemFromCart(game.id));
         } else {
-            dispatch(setItemInCart(game));
+            setCartInDataBase()
+            dispatch(setItemInCart(game))
+            
         }
+    }
+     
+    function setCartInDataBase() {
+        items.map((users) => {
+            set(newCartRef, {
+                    image: users.image,
+                    title: users.title,
+                    video: users.video,
+                    genres: users.genres,
+                    price: users.price,
+                    id: users.id,
+                    description: users.description
+                
+            })
+            .then(() => console.log("write in BD"))
+            .catch((console.error()))
+        })     
     }
     
     return (
--- src/components/log-in/login/login.jsx
+++ src/components/log-in/login/login.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { getAuth, setPersistence, signInWithEmailAndPassword, browserSessionPersistence } from "firebase/auth";
-import { useDispatch } from 'react-redux';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getDatabase, ref, child, get, onValue  } from "firebase/database";
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { LoginForm } from '../login-form/login-form';
 import { setUser } from '../../../redux/users/reducer';
+import { setItemInCart } from '../../../redux/cart/reducer';
 
 export const Login = () => {
     const dispatch = useDispatch();
     const { push } = useHistory();
+    const { id } = useSelector((state) => state.user);
+    const dataBaseRef = ref(getDatabase());
+
+    get(child(dataBaseRef, "user: " + id))
+            .then((snapshot) => {
+                if (snapshot.exists()) {
+                    const values = snapshot.val();
+                    for(let val in values) {
+                        dispatch(setItemInCart(values[val]))
+                    }
+                } else {
+                    console.log("No data available");
+                }
+            })
+            .catch((error) => { alert(error) });
 
     const handleLogin = (email, password) => {
         const auth = getAuth();
@@ -18,21 +35,12 @@ export const Login = () => {
                     token: user.accessToken,
                     id: user.uid, 
                 }));
-            })
-            .then(() => {
-                setPersistence(auth, browserSessionPersistence)
-                    .then((event) => {
-                        console.log(auth)
-                        return signInWithEmailAndPassword(auth, email, password)
-                    })
-                    .catch((error) => {
-                        alert(error.code);
-                    })
-                    push('/');
-            })
+                push('/');
+            })   
             .catch(() => alert('Invaild user!'))
-        
+            
     }
+    
     return (
         <LoginForm
             title='Войти'
